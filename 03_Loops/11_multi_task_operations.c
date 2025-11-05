eca564cf18607f6ddc672db302c296f84808fc2a
shubham21-star <shubham.asp01@gmail.com>
2025-11-05 15:32:27 +0530

    trying do output in one line

--- 03_Loops/11_multi_task_operations.c
+++ 03_Loops/11_multi_task_operations.c
@@ -3,51 +3,47 @@
 int main() {
     int year, month;
 
-    printf("Enter year: ");
-    scanf("%d", &year);
+    printf("Enter year and month number (1-12) separated by space: ");
+    scanf("%d %d", &year, &month);
 
-    printf("Enter month number (1-12): ");
-    scanf("%d", &month);
 
     // Leap year check
     int isLeap = 0;
     if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
         isLeap = 1;
 
-    // Months data
     char *months[12] = {"January", "February", "March", "April", "May", "June",
                         "July", "August", "September", "October", "November", "December"};
     int days[12] = {31, (isLeap ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    // Validation
     if (month >= 1 && month <= 6) {
-        printf("\n%s (%d days, %d)", months[month - 1], days[month - 1], year);
+        printf("Enter year and month number (1-12) separated by space: %d %d --> %s (%d days, %d) ", 
+               year, month, months[month - 1], days[month - 1], year);
     } 
     else if (month == 12) {
-        printf("\nDiwali: October 20, 2025 | Vacation: October 18 to 26, 2025 | Dates: ");
+        printf("Enter year and month number (1-12) separated by space: %d %d -->  ", year, month);
+        printf(" -->Diwali: October 20, 2025 | Vacation: October 18 to 26, 2025 | Dates: ");
         for (int i = 18; i <= 26; i++) {
             printf("Oct %d ; ", i);
         }
-        printf("2025");
-
-        printf("\n\nInfinite mode activated — December selected.\n");
+        printf("2025 Infinite mode activated — December selected. ");
         while (1) {
             printf("I am the best programmer alive! ");
         }
     } 
     else {
-        printf("\nWrong input! Only first 6 months are valid.");
+        printf("Enter year and month number (1-12) separated by space: %d %d -->Wrong input! Only first 6 months are valid. ",
+               year, month);
     }
 
-    // Diwali vacation always printed (for every year)
     if (year == 2025) {
-        printf("\n\nDiwali: October 20, 2025 | Vacation: October 18 to 26, 2025 | Dates: ");
+        printf("-->Diwali: October 20, 2025 | Vacation: October 18 to 26, 2025 | Dates: ");
         for (int i = 18; i <= 26; i++) {
-            printf("Oct %d ;  ", i);
+            printf("Oct %d ; ", i);
         }
         printf("%d", year);
     } else {
-        printf("\n\nNo Diwali data available for %d.", year);
+        printf("-->No Diwali data available for %d.", year);
     }
 
     return 0;
