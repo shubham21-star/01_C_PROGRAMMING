10a1318dcf57bf80ada62f1c5cccf77be7657934
shubham21-star <shubham.asp01@gmail.com>
2025-11-14 12:15:10 +0530

    update 07_reverse_number.c

--- 03_Loops/07_reverse_number.c
+++ 03_Loops/07_reverse_number.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
-    int n, rev = 0, rem;
+    int n, rev = 0, rem,temp;
     printf("Enter a number: ");
     scanf("%d", &n);
-
+    temp=n;
     while(n > 0) {
         rem = n % 10;
         rev = rev * 10 + rem;
@@ -11,5 +11,10 @@ int main() {
     }
 
     printf("Reversed number = %d\n", rev);
+    if(temp==rev){
+        printf("the number %d is pallindrom, %d",temp,rev);
+    }else{
+        printf("the number is not pallindrom")
+    }
     return 0;
 }
