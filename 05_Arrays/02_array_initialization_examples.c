e798a46bdd026f9612628ab7dd6efffb78657200
shubham21-star <shubham.asp01@gmail.com>
2025-11-15 21:20:49 +0530

    array_initialization_examples.c

---
+++ 05_Arrays/02_array_initialization_examples.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+// case 1
+int main() {
+    int a[5]={1,2,3,4,5};
+    int i;
+    printf("Enter the array:  ");
+
+     for (i = 0; i <5; i++) {
+        printf("%d ",  a[i]);
+    }
+// case 2
+
+    int b[5]={1,2,3};
+    int i;
+    printf("Enter the array:  ");
+
+     for (i = 0; i <5; i++) {
+        printf("%d ",  a[i]);
+    }
+
+// case 3 
+
+    int c[5]={1,2,3,4,5,6,7,8};
+    int i;
+    printf("Enter the array:  ");
+
+     for (i = 0; i <5; i++) {
+        printf("%d ",  a[i]);
+    }
+// case 4
+
+    int d[5]={};
+    int i;
+    printf("Enter the array:  ");
+
+     for (i = 0; i <5; i++) {
+        printf("%d ",  a[i]);
+    }
+    return 0;
+}   
